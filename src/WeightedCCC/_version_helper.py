dd4211894b38d0925f41859dc26b7070b534cfaa
David-Araripe <david.araripe17@gmail.com>
2024-03-26 13:33:00 +0100

    lower fallback version

--- src/WeightedCCC/_version_helper.py
+++ src/WeightedCCC/_version_helper.py
@@ -7,5 +7,5 @@ def get_version():
     if (Path(__file__).parent / "_version.py").exists():
         from ._version import __version__  # noqa F401
     else:
-        __version__ = "1.0.1"
+        __version__ = "0.0.1"
     return __version__
